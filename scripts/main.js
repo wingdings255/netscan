4dd971d99fad8068d1519ce06b0d0d4bf666ca15
wingdings255 <wingdings255@gmail.com>
2020-07-30 00:51:41 -0700

    Cleaned up index.html and main.js changed hosts up and down vars added bannerStats function

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,17 @@
 var $ = function(id) { return document.getElementById(id); }
 
-var hostsAlive = 0;
+var hostsUp = 5;//hardcoding for testing
+var hostsDown = 0;
 var ipAddr;
 
+var bannerStats = function(){
+    $('up').innerHTML = "Hosts up:" + hostsUp;
 
+}
 
 
 window.onload = function() {
 //generates banner info
-
+    bannerStats();
 
 }
