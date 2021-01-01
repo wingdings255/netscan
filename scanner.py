eb6fc249a1c6ced73c0374a8ccb035e9d80d831a
wingdings255 <46852882+wingdings255@users.noreply.github.com>
2021-01-01 01:32:04 +0000

    Updated scanner.py
    Added ping check to eliminate offline hosts from scanning list

--- scanner.py
+++ scanner.py
@@ -1,71 +1,38 @@
-import socket
-import os
-import json
+# import socket
+# import os
+# import json
+import ipcalc
 import subprocess
-import time
 from datetime import datetime
 
 # Vars
-netid = '192.168.20.'
-netmask = '24'
+netid = '192.168.1.0/24'  # For testing
 global iplst
 iplst = []
 alive = []
-logging = False
-
-if netmask == '24':
-    netrange = '255'
-
-class eventStream:
-    # Logging class object
-    def outCons(msg):
-        # prints log to console
-        print('[datetime.now()]::[' + msg + ']')
-    def outFile(msg):
-        # Prints log to log file
-    def log(msg):
-        if logging = True:
-            outCons(msg)
-        else:
-            outFile(msg)    
-
 
 
 def main():
-    # print("[+]: Generating the host list")
-    eventStream.log("Generating the host list")
-    for i in range(1, int(netrange)):
-        ip = netid + str(i)
-        iplst.append(ip)
-    print("[+]: IP list contains " + str(len(iplst)) + " hosts")
-    scanner(iplst)
-
-
-def scanner(iplst):
+    iplst = ipcalc.Network(netid)
+    print("[+]:: IP list contains " + str(len(iplst)) + " hosts")
     t1 = datetime.now()
     for i in iplst:
-        currentip = i
-        getalive(currentip, iplst)
-        print("[+]: Found " + str(len(alive)) + " alive hosts")
+        getAlive(i)
     t2 = datetime.now()
     time = t2 - t1
-    print("[?]: Scan took [" + str(time) + "] to scan " + str(len(iplst)) + " hosts")
+    print("[!]:: Scan took " + time)
 
 
-def getalive(currentip, iplst):
-    # print("[*]: Scanning " + str(currentip))
-    test = subprocess.call(['ping', '-n', '1', currentip])
-    if test == '0':
-        alive.append(currentip)
-        print("[+]: " + currentip + " is alive")
-    elif test == '1':
-        print("[!]: Host isnt alive")
-
-def scan(currentip):
-    print("")    
-
-def getbanner(currentip):
-    print("[*]: Getting banner for" + currentip)
+# Tests to see if host is alive with ping
+def getAlive(host):
+    addr = str(host)
+    print("[+]:: Checking " + addr, end='-->STATUS::[')
+    test = subprocess.call(['ping', '-c', '1', addr], stdout=subprocess.DEVNULL)
+    if test == 0:
+        alive.append(host)  # NOTE: Saved as [IP('192.168.0.1'),]
+        print(" ALIVE ]")
+    elif test == 1:
+        print(" DEAD ]")
 
 
 main()
